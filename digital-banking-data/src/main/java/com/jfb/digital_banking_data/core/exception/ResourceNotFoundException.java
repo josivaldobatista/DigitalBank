dd56fdb2c1425db413d48b6d21a2a040913a2c17
Josivaldo Batista <josivaldo-fb@outlook.com>
2024-11-20 08:14:19 -0300

    DIGITALBANKING04: Exception Handler

---
+++ digital-banking-data/src/main/java/com/jfb/digital_banking_data/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jfb.digital_banking_data.core.exception;
+
+public class ResourceNotFoundException  extends RuntimeException {
+    public ResourceNotFoundException (String message) {
+        super(message);
+    }
+}
