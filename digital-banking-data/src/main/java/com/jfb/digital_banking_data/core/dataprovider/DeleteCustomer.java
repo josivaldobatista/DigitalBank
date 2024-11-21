a2ad28d24b7afc184287b625782cd7e78523511d
Josivaldo Batista <josivaldo-fb@outlook.com>
2024-11-21 09:53:45 -0300

    DIGITALBANKING06: Added logical delete to customer

---
+++ digital-banking-data/src/main/java/com/jfb/digital_banking_data/core/dataprovider/DeleteCustomer.java
@@ -0,0 +1,6 @@
+package com.jfb.digital_banking_data.core.dataprovider;
+
+public interface DeleteCustomer {
+
+    void logicalDelete(String id);
+}
---
+++ digital-banking-data/src/main/java/com/jfb/digital_banking_data/core/usecase/DeleteCustomerUseCase.java
@@ -0,0 +1,6 @@
+package com.jfb.digital_banking_data.core.usecase;
+
+public interface DeleteCustomerUseCase {
+
+    void execute(String id);
+}
