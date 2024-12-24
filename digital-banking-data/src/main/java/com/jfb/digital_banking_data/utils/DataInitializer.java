143056f25377daf3bcebd6e56c1939f5d5fbd6fe
Josivaldo Batista <josivaldo-fb@outlook.com>
2024-12-24 11:15:57 -0300

    DIGITALBANKING-029/BUGFIX: Fixed issue with persistent customer duplication error state.

--- digital-banking-data/src/main/java/com/jfb/digital_banking_data/utils/DataInitializer.java
+++ digital-banking-data/src/main/java/com/jfb/digital_banking_data/utils/DataInitializer.java
@@ -3,7 +3,9 @@ package com.jfb.digital_banking_data.utils;
 import com.jfb.digital_banking_data.core.domain.Account;
 import com.jfb.digital_banking_data.core.domain.Customer;
 import com.jfb.digital_banking_data.core.domain.Status;
+import com.jfb.digital_banking_data.core.usecase.account.FindAllAccountUseCase;
 import com.jfb.digital_banking_data.core.usecase.account.InsertAccountUseCase;
+import com.jfb.digital_banking_data.core.usecase.customer.FindAllCustomerUseCase;
 import com.jfb.digital_banking_data.core.usecase.customer.InsertCustomerUseCase;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -18,52 +20,61 @@ public class DataInitializer implements CommandLineRunner {
 
     private final InsertCustomerUseCase insertCustomerUseCase;
     private final InsertAccountUseCase insertAccountUseCase;
+    private final FindAllCustomerUseCase findAllCustomerUseCase;
+    private final FindAllAccountUseCase findAllAccountUseCase;
 
-    public DataInitializer(InsertCustomerUseCase insertCustomerUseCase, InsertAccountUseCase insertAccountUseCase) {
+    public DataInitializer(
+            InsertCustomerUseCase insertCustomerUseCase,
+            InsertAccountUseCase insertAccountUseCase,
+            FindAllCustomerUseCase findAllCustomerUseCase,
+            FindAllAccountUseCase findAllAccountUseCase) {
         this.insertCustomerUseCase = insertCustomerUseCase;
         this.insertAccountUseCase = insertAccountUseCase;
+        this.findAllCustomerUseCase = findAllCustomerUseCase;
+        this.findAllAccountUseCase = findAllAccountUseCase;
     }
 
     @Override
     public void run(String... args) {
-//        // Verifica se já existe um Customer no banco de dados
-//        var customer1 = new Customer();
-//        customer1.setId("f4028655-2ccf-4493-8808-48c2ddf7cd62");
-//        customer1.setName("John Doe");
-//        customer1.setEmail("johndoe@example.com");
-//        customer1.setBirthDate(LocalDate.parse("1980-01-01"));
-//        customer1.setCpfCnpj("72473107076");
-//        customer1.setStatus(Status.ATIVO);
-//        insertCustomerUseCase.execute(customer1);
-//
-//        var account1 = new Account();
-//        account1.setId("45281bb5-8c85-4a44-b894-aada6f2be05c");
-//        account1.setBalance(new BigDecimal("15000.00"));
-//        account1.setBranch("001");
-//        account1.setCustomerId("f4028655-2ccf-4493-8808-48c2ddf7cd62");
-//        account1.setStatus(Status.ATIVO);
-//        account1.setCpfCnpj("72473107076");
-//        account1.setAccountNumber("00000001");
-//        insertAccountUseCase.execute(account1);
-//
-//        var customer2 = new Customer();
-//        customer2.setId("9aa355aa-aa67-46fa-a732-d777efc9e996");
-//        customer2.setName("Maria Mariana");
-//        customer2.setEmail("maria@example.com");
-//        customer2.setBirthDate(LocalDate.parse("1984-01-01"));
-//        customer2.setCpfCnpj("22730143092");
-//        customer2.setStatus(Status.ATIVO);
-//        insertCustomerUseCase.execute(customer2);
-//
-//        var account2 = new Account();
-//        account2.setId("cd96dff5-e998-41f9-bfce-155df0371ddd");
-//        account2.setBalance(new BigDecimal("20000.00"));
-//        account2.setBranch("001");
-//        account2.setCustomerId("9aa355aa-aa67-46fa-a732-d777efc9e996");
-//        account2.setStatus(Status.ATIVO);
-//        account2.setCpfCnpj("22730143092");
-//        account2.setAccountNumber("00000001");
-//        insertAccountUseCase.execute(account2);
+        // Verifica se já existem registros no banco de dados
+        if (findAllCustomerUseCase.execute().isEmpty() && findAllAccountUseCase.execute().isEmpty()) {
+            var customer1 = new Customer();
+            customer1.setId("f4028655-2ccf-4493-8808-48c2ddf7cd62");
+            customer1.setName("John Doe");
+            customer1.setEmail("johndoe@example.com");
+            customer1.setBirthDate(LocalDate.parse("1980-01-01"));
+            customer1.setCpfCnpj("72473107076");
+            customer1.setStatus(Status.ATIVO);
+            insertCustomerUseCase.execute(customer1);
+
+            var account1 = new Account();
+            account1.setId("45281bb5-8c85-4a44-b894-aada6f2be05c");
+            account1.setBalance(new BigDecimal("15000.00"));
+            account1.setBranch("001");
+            account1.setCustomerId("f4028655-2ccf-4493-8808-48c2ddf7cd62");
+            account1.setStatus(Status.ATIVO);
+            account1.setCpfCnpj("72473107076");
+            account1.setAccountNumber("00000001");
+            insertAccountUseCase.execute(account1);
+
+            var customer2 = new Customer();
+            customer2.setId("9aa355aa-aa67-46fa-a732-d777efc9e996");
+            customer2.setName("Maria Mariana");
+            customer2.setEmail("maria@example.com");
+            customer2.setBirthDate(LocalDate.parse("1984-01-01"));
+            customer2.setCpfCnpj("22730143092");
+            customer2.setStatus(Status.ATIVO);
+            insertCustomerUseCase.execute(customer2);
+
+            var account2 = new Account();
+            account2.setId("cd96dff5-e998-41f9-bfce-155df0371ddd");
+            account2.setBalance(new BigDecimal("20000.00"));
+            account2.setBranch("001");
+            account2.setCustomerId("9aa355aa-aa67-46fa-a732-d777efc9e996");
+            account2.setStatus(Status.ATIVO);
+            account2.setCpfCnpj("22730143092");
+            account2.setAccountNumber("00000001");
+            insertAccountUseCase.execute(account2);
+        }
     }
 }
-
