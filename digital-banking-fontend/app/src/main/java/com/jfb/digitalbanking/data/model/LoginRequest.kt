550dea3110efe835e5d3f7220647bf83f028edeb
Josivaldo Batista <josivaldo-fb@outlook.com>
2026-03-19 12:16:55 -0300

    remove digital-banking-fontend do repositório (será separado em outro repo)

--- digital-banking-fontend/app/src/main/java/com/jfb/digitalbanking/data/model/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.jfb.digitalbanking.data.model
-
-data class LoginRequest(
-    val username: String? = null,
-    val password: String
-)
--- digital-banking-fontend/app/src/main/java/com/jfb/digitalbanking/data/model/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.jfb.digitalbanking.data.model
-
-data class LoginResponse(
-    val token: String
-)
